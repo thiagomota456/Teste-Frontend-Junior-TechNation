c58d00ef26819da500d9c2501d19d63a1ef8231e
Thiago Soares Mota <thiagomota456@gmail.com>
2024-02-14 23:13:34 -0300

    Refatoração

--- script/index/script.js
+++ script/index/script.js
@@ -3,32 +3,43 @@ function setMenu(menuName){
 }
 
 function toggleClass(event) {
+    clearListaColunaEsquerda();
+    marcarListaColunaEsquerda(event.currentTarget);
+    setMenu(clickedLink.textContent.trim());
+}
+
+function marcarListaColunaEsquerda(clickedLi){
+    var clickedLink = clickedLi.querySelector('a');
+    clickedLi.classList.add('selected-lista-coluna-esquerda');
+    clickedLink.classList.add('colorir-link');
+}
+
+function clearListaColunaEsquerda(){
     var lis = document.querySelectorAll('#lista-coluna-esquerda li');
     for (var i = 0; i < lis.length; i++) {
         lis[i].classList.remove('selected-lista-coluna-esquerda');
         lis[i].querySelector('a').classList.remove('colorir-link');
     }
-
-    var clickedLi = event.currentTarget; 
-    var clickedLink = clickedLi.querySelector('a');
-    clickedLi.classList.add('selected-lista-coluna-esquerda');
-    clickedLink.classList.add('colorir-link');
-    setMenu(clickedLink.textContent.trim());
 }
 
 function toggleClasscListaColapse(event) {
+    clearListaColapse();
+    marcarListaColapse(event.target.closest("li"));
+    setMenu(elementoClicado.textContent.trim());
+}
+
+function marcarListaColapse(elementoClicado){
+    elementoClicado.id = "item-da-lista-colapse-selecionado";
+}
+
+function clearListaColapse(){
     var elementoSelecionadoAnteriormente = document.getElementById("item-da-lista-colapse-selecionado");
     if (elementoSelecionadoAnteriormente) {
         elementoSelecionadoAnteriormente.removeAttribute("id");
     }
-    
-    var elementoClicado = event.target.closest("li");
-    elementoClicado.id = "item-da-lista-colapse-selecionado";
-    setMenu(elementoClicado.textContent.trim());
 }
 
 function setConteudo(pagePath){
-
     documen.querySelector('#conteudo') = "";
     $("#conteudo").load(pagePath);
 }
@@ -57,6 +68,20 @@ window.addEventListener('load', function() {
         //console.log('A página está sendo recarregada.');
     }
 });
+
+function buscarTexto(elementoPai, texto) {
+    var elementos = elementoPai.querySelectorAll('li');
+    var resultados = [];
+
+    elementos.forEach(function(elemento) {
+        var conteudo = elemento.textContent || elemento.innerText;
+        if (conteudo.indexOf(texto) !== -1) {
+            resultados.push(elemento);
+        }
+    });
+
+    return resultados;
+}
 
 //Inicilização defalt por enqquanto
 //$("#conteudo").load("../../lista_de_notas_emitidas.html");
